4f83ea4fbd53f91bc51b02bd94cafb73ce0a5197
guryl29 <111236089+guryl29@users.noreply.github.com>
2022-09-09 14:59:19 +0300

    HW

---
+++ number41/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите размер массива:");
+int size = int.Parse(Console.ReadLine());
+
+int [] array = new int [size];
+int count = 0;
+
+for ( int i=0; i<array.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива под индексом {i}");
+    array[i]= int.Parse(Console.ReadLine());
+}
+Console.Write("Массив: ");
+Console.Write("[");
+
+Console.Write(string.Join(",", array));
+Console.Write("]");
+
+for(int i=0; i<array.Length; i++)
+{
+    if(array[i]>0)
+    {
+        count=count + 1;
+    }  
+
+}
+Console.WriteLine(" количество положитльных чисел в массиве = " + count );
+
---
+++ number43/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("введите значение b1");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("введите значение k1");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("введите значение b2");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("введите значение k2");
+double k2 = double.Parse(Console.ReadLine());
+
+if ((b1 == b2) && (k1 == k2))
+{
+    Console.WriteLine("прямые не пересекаются");
+}
+else
+{
+    double x = (b2-b1)/(k1-k2);
+double y = k1 * x + b1;
+
+Console.WriteLine("прямые пересекаются в точке с координатами"+"("+ x +";"+ y +")");
+}
